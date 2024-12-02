97d86d007cc3d9ddd8b881aca6bb113e9f8380b3
muunji <twoji3019@naver.com>
2024-12-02 15:16:24 +0900

    길이수정

---
+++ dot.js
@@ -0,0 +1,31 @@
+let dot = document.getElementById('dot');
+let length = 300; 
+//한 번에 이동할 거리
+let x=0, y=0;
+let direction = 'right';
+//처음에는 오른쪽으로 이동
+
+function moveDot() {
+  if(direction === 'right') {
+    x += length;
+    direction = 'down';
+    //오른쪽 이동 후 아래로 이동
+  } else if (direction === 'down') {
+    y+=length;
+    direction = 'left';
+  } else if (direction === 'left') {
+    x -= length;
+    direction = 'up';
+  } else if (direction === 'up') {
+    y-=length;
+    direction = 'right'
+  }
+
+  //점 위치 설정
+  dot.style.left = x+'px';
+  dot.style.top = y+'px';
+}
+
+//점을 반복적으로 이동
+//비동기
+setInterval(moveDot,1000);
